fd423efeedf782b9168169870fd07dc291a4fb01
QuyenVN <quyen.vungoc@amela.vn>
2023-07-27 16:24:26 +0700

    Add api title

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,20 @@
 import React, { PureComponent } from "react";
 import "./Header.css";
+import configs from "./config";
 
 export default class Header extends PureComponent {
+  state = {
+    title: "",
+  };
+
+  componentDidMount() {
+    fetch(`${configs.API_DOMAIN}/api/title`)
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({ title: data });
+      });
+  }
+
   render() {
     return (
       <header className="component-header">
@@ -11,7 +24,7 @@ export default class Header extends PureComponent {
           height="32"
           alt=""
         />
-        Emoji Search
+        {this.state.title}
         <img
           src="//cdn.jsdelivr.net/emojione/assets/png/1f63a.png"
           width="32"
---
+++ src/config.js
@@ -0,0 +1,5 @@
+const configs = {
+  API_DOMAIN: process.env.REACT_APP_API_DOMAIN,
+};
+
+export default configs;
