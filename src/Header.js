d41b1c36ac6184a140d479b3d7363aa11cedb64e
vinh.nguyenthanh <vinh.nguyenthanh@amela.vn>
2023-07-27 16:35:50 +0700

    fix get title from api

--- src/Header.js
+++ src/Header.js
@@ -11,7 +11,7 @@ export default class Header extends PureComponent {
     fetch(`${configs.API_DOMAIN}/api/title`)
       .then((res) => res.json())
       .then((data) => {
-        this.setState({ title: data });
+        this.setState({ title: data?.title });
       });
   }
 
